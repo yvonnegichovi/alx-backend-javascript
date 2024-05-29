b387a7f592ada6468a66f70692e1159f90c620bf
yvonnegichovi <yvonnegichovi@gmail.com>
2024-05-29 20:37:32 +0000

    task 10
    asynchronous Peomises and use of await to achieve them. Add an error case for it too

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import { uploadPhoto, createUser } from './utils';
+async function asyncUploadUser() {
+  try {
+    const photoPromise = uploadPhoto();
+    const userPromise = createUser();
+    const [photoResponse, userResponse] = await Promise.all([photoPromise, userPromise]);
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+export default asyncUploadUser;
