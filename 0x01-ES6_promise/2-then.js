bb9e1433e235c9a8f15e0657cad06e18aff10a27
yvonnegichovi <yvonnegichovi@gmail.com>
2024-05-29 11:16:07 +0000

    task 2
    fixing a typing error in the success message

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ function getFullResponseFromAPI(promise) {
   promise
     .then(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'Success' };
+      return { status: 200, body: 'success' };
     })
     .catch(() => {
       console.log('Got a response from the API');
