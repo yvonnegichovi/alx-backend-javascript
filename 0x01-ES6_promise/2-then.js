ab517dd4ecfce3b0bf73a8e8b13096015bd505a8
yvonnegichovi <yvonnegichovi@gmail.com>
2024-05-29 11:11:36 +0000

    task 2
    A promise with a then for fulfilled promise, a catch for error as a result of rejected promise and a message shared to the console for either of the resolutions

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+function getFullResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
+
+export default getFullResponseFromAPI;
