8ea9d913ae4e3df06a08e5d3de6469d9202dc0fc
yvonnegichovi <yvonnegichovi@gmail.com>
2024-05-29 16:14:53 +0000

    task 3
    promise returned from an importation from a prototype

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from "./utils";
+
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoData, userData]) => {
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+    })
+    .catch(error => {
+      console.error("Signup system offline");
+    });
+}
+
+export default handleProfileSignup;
