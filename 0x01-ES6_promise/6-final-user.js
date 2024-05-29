0a65b8ecfebe861e5cfa9ea03c9157cab1e5a7e9
yvonnegichovi <yvonnegichovi@gmail.com>
2024-05-29 17:58:05 +0000

    task 6
    multiple promises, others are imported

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,2 +1,12 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpPromise = signUpUser(fileName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
+}
