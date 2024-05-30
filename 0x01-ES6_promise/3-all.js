942334363fb8aefd912caa14aaf5ba9332b67c06
yvonnegichovi <yvonnegichovi@gmail.com>
2024-05-30 06:23:51 +0000

    task 2.1
    fixing a typing error on the prototype function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  Promise.all([uploadPhoto, createUser])
     .then(([photoData, userData]) => {
       console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
     })
