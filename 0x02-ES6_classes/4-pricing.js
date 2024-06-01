2d16d103eb24c17eac1578c78380d42cd0517804
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-01 10:00:11 +0000

    task 4
    static methods

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,4 +2,44 @@ import Currency from './3-currency';
 
 class Pricing {
   constructor(amount, currency) {
-    this._amount = this._validateNumber(amount, 
+    this._amount = this._validateNumber(amount, 'Amount');
+    this._currency = this._validateCurrency(currency, 'Currency');
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = this._validateNumber(newAmount, 'Amount');
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = this._validateCurrency(newCurrency, 'Currency');
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _validateNumber(value, attributeName) {
+    if (typeof value !== 'number' || Number.isNaN(value)) {
+      throw new TypeError(`${attributeName} must be a number`);
+    }
+    return value;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _validateCurrency(value, attributeName) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError(`${attributeName} must be an instance of Currency`);
+    }
+    return value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+}
+export default Pricing;
