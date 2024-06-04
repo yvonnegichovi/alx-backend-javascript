3900967cafa375079abcd9439d4d533b80d4141e
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-04 20:32:20 +0000

    task 11
    weakMap

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint);
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    } else {
+      weakMap.set(endpoint, count + 1);
+    }
+  }
+}
