b84ee2c45577244265cdb03bb5e003c4f756d1ea
yvonnegichovi <yvonnegichovi@gmail.com>
2024-06-04 18:00:39 +0000

    task 8
    what if we are checking for nothing

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
   const result = Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
