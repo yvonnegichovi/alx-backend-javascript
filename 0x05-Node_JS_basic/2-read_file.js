a6262b2f5a78c5f7967b259a195ead2669ccf3ed
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-23 00:14:32 +0300

    synchronous file with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,9 +3,29 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-  }
-  catch (error) {
-    throw new Error('Cannot load the database');
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
+    const students = {};
+    let totalStudents = 0;
+    for (const line of lines.slice(1)) {
+      const [firstName, , , field] = line.split(',');
+      if (firstName && field) {
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(firstName);
+        totalStudents++;
+      }
+    }
+    console.log(`Number of students ${totalStudents}`);
+    for (const field in students) {
+      const list = students[field].join(', ');
+      console.log(`Number of students in ${field}: ${students[field].length}. List: ${list}`);
+    }
+  } catch (error) {
+    throw new Error(`Cannot load the database`);
   }
 }
 
