05fb1121f98c202fb1146a776698ec154cc874cc
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-26 22:36:04 +0300

    first test file with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,4 +17,19 @@ describe('calculateNumber', () => {
   it('should return 6 when inputs are 1.5 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+  it('should return 5 when the first number is 1.2 and the second number is 3.5', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.5), 5);
+  });
+
+  it('should return 5 when the first number is 1 and the second number is 3.5', () => {
+    assert.strictEqual(calculateNumber(1, 3.5), 5);
+  });
+
+  it('should return 6 when the first number is 1.5 and the second number is 3.5', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.5), 6);
+  });
+
+  it('should return 6 when the first number is 1.4 and the second number is 4.6', () => {
+    assert.strictEqual(calculateNumber(1.4, 4.6), 6);
+  });
 });
