071182da6b3a5b431280f4df89d733719e630c59
yvonnegichovi <yvonnegichovi@gmail.com>
2024-07-26 22:54:44 +0300

    A  test for combined descriptions

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,7 +1,14 @@
 function calculateNumber (type, a, b) {
-  do (return Math.round(a) + Math.round(b)) {
-    when (type === SUM);
-
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+  }
+  return 0;
 }
 
 module.exports = calculateNumber;
